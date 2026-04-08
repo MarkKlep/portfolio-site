af48f00d52b8b732b9fa1917761effddafc4fac4
Mark Klepanchuk <Mark.Klepanchuk@dataart.com>
2026-04-08 16:05:44 +0300

    Remove .next build folder from git and add to .gitignore

--- .next/static/mkNRqkBTQJXakYCXWb_jK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/21-9501220f6d7faa58.js","static/chunks/pages/index-d0b7d7896a84869a.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
