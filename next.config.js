aebc2e29a98f50751ee19266e0fc8cae6c9a0634
Mark Klepanchuk <Mark.Klepanchuk@dataart.com>
2026-04-08 16:25:14 +0300

    Optimize build for memory: disable SWC minify, ESLint, and TypeScript checks during build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       {
